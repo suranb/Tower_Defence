04c51b06c5da9a11b22010194751c5c5fc4d8119
suranb <50323695+suranb@users.noreply.github.com>
2019-11-04 12:47:18 +0100

    Shop - fixed grid

---
+++ Assets/Scripts/BuildManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildManager : MonoBehaviour
+{
+    public static BuildManager instance;
+
+    void Awake()
+    {
+        if (instance != null)
+        {
+            return;
+        }
+        instance = this;
+    }
+
+    public GameObject standardTurretPrefab;
+    public GameObject explosiveTurretPrefab;
+
+    private GameObject turretToBuild;
+    public GameObject GetTurretToBuild ()
+    {
+        return turretToBuild;
+    }
+    public void SetTurretToBuild (GameObject turret)
+    {
+        turretToBuild = turret;
+    }
+}
---
+++ Assets/Scripts/buildingGrid.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine.EventSystems;
+using UnityEngine;
+
+public class Grid : MonoBehaviour
+{
+    public Color hoverColor;
+    public Vector3 positionOffset;
+
+    private GameObject turret;
+
+    private Renderer rend;
+    private Color startColor;
+    BuildManager buildManager;
+    void start ()
+    {
+        rend = GetComponent<Renderer>();
+        startColor = rend.material.color;
+        buildManager = BuildManager.instance;
+    }
+
+    void onMouseDown ()
+    {
+        if (EventSystem.current.IsPointerOverGameObject())
+            return;
+
+        if (buildManager.GetTurretToBuild() == null)
+        {
+            return;
+        }
+
+        if (turret != null)
+        {
+            return;
+        }
+
+        GameObject turretToBuild = buildManager.GetTurretToBuild();
+        turret = (GameObject)Instantiate(turretToBuild, transform.position + positionOffset, transform.rotation);
+    }
+
+    void OnMouseEnter ()
+    {
+        if (EventSystem.current.IsPointerOverGameObject())
+            return;
+
+        if (buildManager.GetTurretToBuild() == null)
+        {
+            return;
+        }
+        rend.material.color = hoverColor;
+    }
+
+    void OnMouseExit ()
+    {
+        rend.material.color = startColor;
+    }
+}
+
